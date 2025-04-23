1bfef769cadd1a3e38bb2d85988d5bc9c851ffed
Daeyeon Kim <58037317+kim946509@users.noreply.github.com>
2025-04-23 12:39:55 +0900

    fix : favicon 수정

--- assets/js/components/header-jp.js
+++ assets/js/components/header-jp.js
@@ -20,7 +20,7 @@ class HeaderJp extends HTMLElement {
       <header id="header" class="header d-flex align-items-center sticky-top">
         <div class="container position-relative d-flex align-items-center">
           <a href="index.html" class="logo d-flex align-items-center me-auto">
-            <img src="../assets/img/logo.jpg" alt="">
+            <img src="../assets/img/logo-en.jpg" alt="">
           </a>
 
           <nav id="navmenu" class="navmenu">
